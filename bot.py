d3d2b7b5799c98eb9adea3b9f3a4c47b36bf0672
gaponukz <gaponukz54@gmail.com>
2022-06-25 01:03:25 -0700

    Update

--- bot.py
+++ bot.py
@@ -1,21 +1,35 @@
 import os
 import json
 import asyncio
+from pyrsistent import s
 
 from web3 import Web3
 from web3 import HTTPProvider
 
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
+
+class UsersList(list):
+    def append(self, __object) -> None:
+        with open('signed_users.json', 'w', encoding='utf-8') as out:
+            super().append(__object)
+            json.dump(self, out, indent=4)
 
 load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
 web3 = Web3(HTTPProvider(os.getenv('WEB3PROVIDER')))
 
 with open('contract.json', 'r', encoding='utf-8') as out:
     contract = json.load(out)
     contract = web3.eth.contract(address=contract['address'], abi=contract['abi'])
+
+with open('languages.json', 'r', encoding='utf-8') as out:
+    languages = json.load(out)
+
+with open('signed_users.json', 'r', encoding='utf-8') as out:
+    signed_users = UsersList(json.load(out))
 
 async def log_loop(event_filters, poll_interval):
     while True:
@@ -38,6 +52,19 @@ async def handle_event(event):
 
     await bot.send_message('1052311571', message)
 
+@dp.message_handler(commands="start")
+async def cmd_test1(message: types.Message):
+    filter_users = [user for user in signed_users if user['id'] == message.from_id]
+
+    if not filter_users:
+        user = {"id": message.from_id, "language": "en"}
+        signed_users.append(user)
+    
+    else:
+        user = filter_users[0]
+
+    await message.reply(languages[user['language']]["greating"])
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     event_filters = [
@@ -45,4 +72,5 @@ if __name__ == "__main__":
         contract.events.WinnerPayment.createFilter(fromBlock='latest')
     ]
 
-    loop.run_until_complete(asyncio.gather(log_loop(event_filters, 2)))
+    loop.create_task(log_loop(event_filters, 2))
+    executor.start_polling(dp, skip_updates=True)
